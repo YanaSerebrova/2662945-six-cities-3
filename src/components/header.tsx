2d1e139d972fe6366305a49706245dc3d5155cc9
Яна <leksa87@inbox.ru>
2026-07-29 18:44:54 +0300

    module3-task1

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,9 @@
-export function Header({
-  isAuth = true,
-  favoritesCount = 0,
-}: {
-  isAuth?: boolean;
+type HeaderProps = {
+  isAuthorized: boolean;
   favoritesCount?: number;
-}) {
+};
+
+export function Header({ isAuthorized, favoritesCount = 0 }: HeaderProps) {
   return (
     <header className="header">
       <div className="container">
@@ -21,35 +20,33 @@ export function Header({
             </a>
           </div>
 
-          {isAuth ? (
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#todo">
-                    <div className="header__avatar-wrapper user__avatar-wrapper" />
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <span className="header__favorite-count">{favoritesCount}</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#todo">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          ) : (
-            <nav className="header__nav">
-              <ul className="header__nav-list">
+          <nav className="header__nav">
+            <ul className="header__nav-list">
+              {isAuthorized ? (
+                <>
+                  <li className="header__nav-item user">
+                    <a className="header__nav-link header__nav-link--profile" href="#todo">
+                      <div className="header__avatar-wrapper user__avatar-wrapper" />
+                      <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                      <span className="header__favorite-count">{favoritesCount}</span>
+                    </a>
+                  </li>
+                  <li className="header__nav-item">
+                    <a className="header__nav-link" href="#todo">
+                      <span className="header__signout">Sign out</span>
+                    </a>
+                  </li>
+                </>
+              ) : (
                 <li className="header__nav-item user">
                   <a className="header__nav-link header__nav-link--profile" href="#todo">
                     <div className="header__avatar-wrapper user__avatar-wrapper" />
                     <span className="header__login">Sign in</span>
                   </a>
                 </li>
-              </ul>
-            </nav>
-          )}
+              )}
+            </ul>
+          </nav>
         </div>
       </div>
     </header>

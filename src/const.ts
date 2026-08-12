9244b1e70e6d99f412b70ab8876b18ac9cc7f163
Яна <leksa87@inbox.ru>
2026-08-12 18:48:32 +0300

    module5-task2 исправила линтер

--- src/const.ts
+++ src/const.ts
@@ -14,6 +14,12 @@ export const OFFER_COUNT = 5;
 
 export const DEFAULT_ZOOM = 12;
 
+export const NEAR_PLACES_COUNT = 3;
+
+export type SortType = 'Popular' | 'Price: low to high' | 'Price: high to low' | 'Top rated first';
+
+export const cities = ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'] as const;
+
 export const ratingTitles: Record<string, string> = {
   '5': 'perfect',
   '4': 'good',
